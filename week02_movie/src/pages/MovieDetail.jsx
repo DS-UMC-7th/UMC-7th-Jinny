cac43401357b951d259866c71a30b7d9dbaf3129
jinhyo <kjhyo0220@gmail.com>
2024-10-29 02:06:25 +0900

    #11 상세 페이지 정보 받아오기

--- week02_movie/src/pages/MovieDetail.jsx
+++ week02_movie/src/pages/MovieDetail.jsx
@@ -1,5 +1,57 @@
+import { useParams } from "react-router-dom";
+import useCustomFetch from "../hooks/useCustomFetch";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { axiosInstance } from "../apis/axios-instance";
+
 const MovieDetail = () => {
-  return <div style={{ color: "white" }}>detail 페이지</div>;
+  const { movieId } = useParams();
+
+  const MovieDetailURL = `/movie/${movieId}?language=ko`;
+
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      try {
+        const response = await axiosInstance.get(MovieDetailURL);
+        setMovie(response.data);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovieDetail();
+  }, [movieId]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>😯로딩 중 입니다...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 style={{ color: "white" }}>🤔에러가 발생했습니다...</h1>
+      </div>
+    );
+  }
+
+  console.log(movie);
+
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>영화 상세 페이지 {movieId}</h1>
+      <h1 style={{ color: "white" }}>영화 상세 페이지 {movie.title}</h1>
+    </div>
+  );
 };
 
 export default MovieDetail;
